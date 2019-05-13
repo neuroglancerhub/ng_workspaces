c3eb0fc95bae0d96bc66a03eea3aa0b704ff307d
Jody Clements <jody@neomorphic.co.uk>
2019-05-13 16:36:50 -0400

    Adds workspace selection to navigation menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
 
 import './App.css';
 
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <div className="App">
-        <Navbar />
+        <Navbar history={history}/>
         <Route path="/" exact component={Home} />
+        {/* TODO: workout how the routes will be used to view alternative workspaces */}
         <Route path="/about" exact component={About} />
       </div>
     </Router>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,71 @@
-import React from 'react';
-import  { Link } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { Link } from "react-router-dom";
+import Select from "react-select";
 
-function Navbar() {
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  search: {
+    fontFamily: theme.typography.fontFamily,
+    width: "15em",
+    marginLeft: "2em"
+  },
+  title: {
+    color: "#fff",
+    textDecoration: "none"
+  }
+}));
+
+const selectStyles = {
+  placeholder: () => ({
+    color: "#fff"
+  }),
+  singleValue: provided => ({
+    ...provided,
+    color: "#fff"
+  }),
+  menu: provided => ({
+    ...provided,
+    color: "#333"
+  }),
+  control: provided => ({
+    ...provided,
+    background: "#3f51b5",
+    border: "1px solid #fff"
+  })
+};
+
+function Navbar(props) {
+  const { history } = props;
+  const classes = useStyles();
+
+  const workspaceOptions = ["neuroglancer", "image picker"].map(dataset => ({
+    value: dataset.replace(/ /, "_"),
+    label: dataset
+  }));
+
+  function handleChange(selected) {
+    // redirect to the workspace that was chosen.
+    history.push(`/${selected.value}`);
+  }
   return (
     <AppBar position="static">
       <Toolbar>
-        <Link to="/">
+        <Link to="/" className={classes.title}>
           <Typography variant="h6" color="inherit">
             neurohub
           </Typography>
         </Link>
-        <Typography variant="body1" color="inherit">
-          select box for workspace
-        </Typography>
+        <Select
+          className={classes.search}
+          styles={selectStyles}
+          onChange={handleChange}
+          placeholder="Select a workspace"
+          options={workspaceOptions}
+        />
       </Toolbar>
     </AppBar>
   );
