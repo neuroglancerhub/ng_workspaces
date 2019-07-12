4e63421f3d2926850b5c640db5dce94ee2bef711
Jody Clements <jody@neomorphic.co.uk>
2019-07-12 15:42:53 -0400

    Adds server url form to ImagePicker workspace.
    The workspace initially loads, asking for a server url to use. This is
    stored in the state and then the page is reloaded to show the
    neuroglancer view and other contents.

--- src/ImagePicker.jsx
+++ src/ImagePicker.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const styles = {
   window: {
     width: "90%",
     margin: "auto",
     height: "500px"
+  },
+  textField: {
+    width:'50%'
+  },
+  inputForm: {
+    margin: '1em'
   }
 };
 
 class ImagePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      serverUrl: null,
+      submitted: false
+    };
+  }
+
   componentDidMount() {
     const { actions } = this.props;
     actions.initViewer({
@@ -41,8 +57,44 @@ class ImagePicker extends React.Component {
       layout: "xz"
     });
   }
+
+  handleChange = url => {
+    this.setState({ serverUrl: url });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // TODO: probably want to validate the url here first.
+    this.setState({ submitted: true });
+  };
+
   render() {
     const { user, children, location, classes } = this.props;
+    const { serverUrl, submitted } = this.state;
+
+    if (!submitted) {
+      return (
+        <div className={classes.inputForm}>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              id="standard-name"
+              label="ServerUrl"
+              className={classes.textField}
+              onChange={event => this.setState({ serverUrl: event.target.value })}
+              margin="normal"
+            />
+            <br />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+      );
+    }
 
     return (
       <div>
@@ -51,6 +103,7 @@ class ImagePicker extends React.Component {
           <p>logged in as: {user.get("userInfo").username}</p>
         )}
         {!user.get("loggedIn") && <p>Not logged in.</p>}
+        <p>ServerUrl: {serverUrl}</p>
         <div className={classes.window}>{children}</div>
         <p>Looking at location: {location.pathname}</p>
         <p>
