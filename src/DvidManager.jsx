421f1ac06f92a2ca5bda24ffbb9289b7419a0a5e
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2020-04-30 21:41:20 -0400

    Sets up DVID with a dialog for the URLs.

--- src/DvidManager.jsx
+++ src/DvidManager.jsx
@@ -7,41 +7,44 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
+const KEY_GRAYSCALE_SOURCE = 'NG_WORKSPACES-FOCUSED-PROOFREADING-GRAYSCALE-SOURCE';
+const KEY_SEGMENTATION_SOURCE = 'NG_WORKSPACES-FOCUSED-PROOFREADING-SEGMENTATION-SOURCE';
+
 export class DvidManager {
   // TODO: Switch to an enum: 0 = not initialized, 1 = initializing, 2 = initialized.
   initialized = 0;
 
   onInitCompleted = undefined;
 
-  segmentationURL = 'https://flyem.dvid.io/d925633ed0974da78e2bb5cf38d01f4d/segmentation';
+  grayscaleURL = 'dvid://https://flyem.dvid.io/ab6e610d4fe140aba0e030645a1d7229/grayscalejpeg';
+
+  segmentationURL = 'dvid://https://flyem.dvid.io/d925633ed0974da78e2bb5cf38d01f4d/segmentation';
 
   init = (onInitCompleted) => {
     if (this.initialized === 0) {
+      if (this.localStorageAvailable()) {
+        // If localStorage is available, use it to remember the URLs across sessions.
+        const g = localStorage.getItem(KEY_GRAYSCALE_SOURCE);
+        if (g) {
+          this.grayscaleURL = g;
+        }
+        const s = localStorage.getItem(KEY_SEGMENTATION_SOURCE);
+        if (s) {
+          this.segmentationURL = s;
+        }
+      }
       this.onInitCompleted = onInitCompleted;
       this.initialized = 1;
     }
   }
 
-  onDialogClosed = () => {
-    this.onInitCompleted();
-  }
+  grayscaleSourceURL = () => (
+    this.grayscaleURL
+  )
 
-  grayscaleSourceURL = () => {
-    // TODO: Get this value from the proper UI.
-    const result = 'dvid://https://flyem.dvid.io/ab6e610d4fe140aba0e030645a1d7229/grayscalejpeg';
-    return (result);
-  }
-
-  segmentationSourceURL = () => {
-    // TODO: Get this value from the proper UI.
-    const result = `dvid://${this.segmentationURL}`;
-    return (result);
-  }
-
-  segmentationAPIURL = () => {
-    const parts = this.segmentationURL.split('/');
-    return (`${parts[0]}//${parts[2]}/api/node/${parts[3]}/${parts[4]}`);
-  }
+  segmentationSourceURL = () => (
+    this.segmentationURL
+  )
 
   sparseVolSize = (bodyId, onCompletion) => {
     const url = `${this.segmentationAPIURL()}/sparsevol-size/${bodyId}`;
@@ -53,6 +56,56 @@ export class DvidManager {
     const url = `${this.segmentationAPIURL()}/label/${key}`;
     fetch(url).then((response) => response.json()).then((data) => onCompletion(data.Label));
   };
+
+  //
+
+  onDialogClosed = () => {
+    this.onInitCompleted();
+  }
+
+  onGrayscaleSourceChange = (event) => {
+    this.grayscaleURL = event.target.value;
+    if (this.localStorageAvailable()) {
+      localStorage.setItem(KEY_GRAYSCALE_SOURCE, this.grayscaleURL);
+    }
+  }
+
+  onSegmentationSourceChange = (event) => {
+    this.segmentationURL = event.target.value;
+    if (this.localStorageAvailable()) {
+      localStorage.setItem(KEY_SEGMENTATION_SOURCE, this.segmentationURL);
+    }
+  }
+
+  segmentationAPIURL = () => {
+    const i = this.segmentationURL.indexOf('http');
+    const url = this.segmentationURL.substring(i);
+    const parts = url.split('/');
+    return (`${parts[0]}//${parts[2]}/api/node/${parts[3]}/${parts[4]}`);
+  }
+
+  localStorageAvailable = () => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+    try {
+      const x = '__storage_test__';
+      localStorage.setItem(x, x);
+      localStorage.removeItem(x);
+      return true;
+    } catch (e) {
+      return e instanceof DOMException && (
+        // everything except Firefox
+        e.code === 22
+        // Firefox
+        || e.code === 1014
+        // test name field too, because code might not be present
+        // everything except Firefox
+        || e.name === 'QuotaExceededError'
+        // Firefox
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+        // acknowledge QuotaExceededError only if there's something already stored
+        && (localStorage && localStorage.length !== 0);
+    }
+  }
 }
 
 export function DvidManagerDialog(props) {
@@ -73,12 +126,21 @@ export function DvidManagerDialog(props) {
 
   // TODO: Add proper UI.
   return (
-    <Dialog onClose={handleClose} open={manager.initialized === 1} disableEnforceFocus>
+    <Dialog onClose={handleClose} open={manager.initialized === 1} maxWidth="md" fullWidth disableEnforceFocus>
       <DialogTitle>Set Up DVID</DialogTitle>
       <DialogContent>
-        <TextField label="Credentials" fullWidth />
-        <TextField label="Server" fullWidth />
-        <TextField label="UUID" fullWidth />
+        <TextField
+          label="Grayscale source URL"
+          defaultValue={manager.grayscaleURL}
+          fullWidth
+          onChange={manager.onGrayscaleSourceChange}
+        />
+        <TextField
+          label="Segmentation source URL"
+          defaultValue={manager.segmentationURL}
+          fullWidth
+          onChange={manager.onSegmentationSourceChange}
+        />
       </DialogContent>
       <DialogActions>
         <Button autoFocus onClick={handleClose} color="primary">
