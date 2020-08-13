d9a97575d741c777f61cd1d04040c23f6b553912
Jody Clements <jody@neomorphic.co.uk>
2020-08-13 11:43:24 -0400

    Converts clio plugin to full page neuroglancer.

--- src/Clio.jsx
+++ src/Clio.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, shallowEqual } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import './Clio.css';
+import './Neuroglancer.css';
 
-const useStyles = makeStyles({
-  window: {
-    width: '90%',
-    margin: 'auto',
-    height: '500px',
-  },
-});
 // eslint-disable-next-line object-curly-newline
 export default function Clio({ children, actions, datasets, selectedDatasetName }) {
   const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
-  const classes = useStyles();
   const dataset = datasets.filter((ds) => ds.name === selectedDatasetName)[0];
 
   useEffect(() => {
@@ -57,28 +48,14 @@ export default function Clio({ children, actions, datasets, selectedDatasetName 
     }
   }, [actions, dataset]);
 
-  return (
-    <div className="clio">
-      <p>Clio</p>
-      {user && (
-        <p>
-          <span>Logged in as:</span>
-          {user.getBasicProfile().getName()}
-        </p>
-      )}
-      {dataset && (
-        <>
-          <p>
-            <b>Dataset:</b>
-            {dataset.name}
-          </p>
-          <div className={classes.window} key={dataset.name}>
-            {children}
-          </div>
-        </>
-      )}
-    </div>
-  );
+  if (dataset) {
+    return (
+      <div className="ng-container">
+        {children}
+      </div>
+    );
+  }
+  return <div />;
 }
 
 Clio.propTypes = {
