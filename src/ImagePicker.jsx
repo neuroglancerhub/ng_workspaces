909ac60198a90ad520a3969d9d8ea37bead8c70f
Jody Clements <jody@neomorphic.co.uk>
2020-08-31 17:22:18 -0400

    Splits ImagePicker transfer & by example results
    These are now two components that deal with the featching and display of
    the results. The image picker just handles passing the location clicked
    in neuroglancer.

--- src/ImagePicker.jsx
+++ src/ImagePicker.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import Radio from '@material-ui/core/Radio';
@@ -7,17 +7,11 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Pagination from '@material-ui/lab/Pagination';
-import Matches from './ImagePicker/Matches';
-import MouseCoordinates from './ImagePicker/MouseCoordinates';
-import { addAlert } from './actions/alerts';
+import ByExampleResults from './ImagePicker/ByExampleResults';
+import TransferResults from './ImagePicker/TransferResults';
 
-const imageSliceUrlTemplate = 'https://tensorslice-bmcp5imp6q-uk.a.run.app/slice/<xyz>/256_256_1/jpeg?location=<location>';
-
-const initialCoordinates = []; // [18416, 16369, 26467];
-const matchesPerPage = 8;
+const initialCoordinates = [24646, 15685, 17376];
 
 const useStyles = makeStyles({
   window: {
@@ -28,66 +22,15 @@ const useStyles = makeStyles({
   matches: {
     margin: '1em',
   },
-  matchText: {
-    textAlign: 'center',
-  },
-  pagination: {
-    textAlign: 'right',
-  },
 });
 
 // eslint-disable-next-line object-curly-newline
 export default function ImagePicker({ actions, datasets, selectedDatasetName, children }) {
   const dataset = datasets.filter((ds) => ds.name === selectedDatasetName)[0];
   const projectUrl = useSelector((state) => state.clio.get('projectUrl'), shallowEqual);
-  const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
-  const dispatch = useDispatch();
   const classes = useStyles();
   const [pickMode, setPickMode] = useState(0);
   const [mousePosition, setMousePosition] = useState(initialCoordinates);
-  const [matches, setMatches] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    if (mousePosition && mousePosition.length > 0 && user && dataset && projectUrl) {
-      // clear the matches before loading the next set
-      setIsLoading(true);
-      setMatches([]);
-      setCurrentPage(1);
-      const options = {
-        headers: {
-          Authorization: `Bearer ${user.getAuthResponse().id_token}`,
-        },
-      };
-
-      const roundedPosition = mousePosition.map((point) => Math.floor(point));
-
-      const signaturesUrl = `${projectUrl}/signatures/likelocation/${dataset.name}?x=${
-        roundedPosition[0]
-      }&y=${roundedPosition[1]}&z=${roundedPosition[2]}`;
-      fetch(signaturesUrl, options)
-        .then((result) => result.json())
-        .then((data) => {
-          if (data.matches) {
-            setMatches(data.matches);
-          }
-          if (data.messsage) {
-            dispatch(
-              addAlert({
-                message: data.messsage,
-                duration: 3000,
-              }),
-            );
-          }
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setIsLoading(false);
-          console.error(error);
-        });
-    }
-  }, [mousePosition, dataset, projectUrl, user, dispatch]);
 
   useEffect(() => {
     if (dataset) {
@@ -122,10 +65,6 @@ export default function ImagePicker({ actions, datasets, selectedDatasetName, ch
     setPickMode(parseInt(event.target.value, 10));
   };
 
-  const handlePageChange = (event, page) => {
-    setCurrentPage(page);
-  };
-
   const callbacks = [
     {
       name: 'coords',
@@ -137,26 +76,21 @@ export default function ImagePicker({ actions, datasets, selectedDatasetName, ch
     },
   ];
 
-  const childrenWithMoreProps = React.Children
-    .map(children, (child) => React.cloneElement(child, { callbacks }, null));
-
-  let imageRootUrl = '';
-
-  if (dataset) {
-    imageRootUrl = imageSliceUrlTemplate.replace(
-      '<location>',
-      dataset.location.replace('gs://', ''),
-    );
-  }
-
-  const pages = Math.ceil(matches.length / matchesPerPage);
-
-  const paginatedList = matches.slice(
-    currentPage * matchesPerPage - matchesPerPage,
-    currentPage * matchesPerPage,
+  const childrenWithMoreProps = React.Children.map(
+    children,
+    (child) => React.cloneElement(child, { callbacks }, null),
   );
-  const matchesText = `Matches ${(currentPage * matchesPerPage - matchesPerPage) + 1} - ${Math.min(currentPage * matchesPerPage, matches.length)} of ${matches.length}`;
 
+  const results = pickMode === 0 ? (
+    <ByExampleResults
+      mousePosition={mousePosition}
+      projectUrl={projectUrl}
+      actions={actions}
+      dataset={dataset}
+    />
+  ) : (
+    <TransferResults mousePosition={mousePosition} dataset={dataset} projectUrl={projectUrl} />
+  );
 
   return (
     <div>
@@ -170,29 +104,20 @@ export default function ImagePicker({ actions, datasets, selectedDatasetName, ch
           value={pickMode}
           onChange={handleChange}
         >
-          <FormControlLabel value={0} control={<Radio color="primary" />} label="Query by Example" />
-          <FormControlLabel value={1} control={<Radio color="primary" />} label="Apply Transfer Network" />
+          <FormControlLabel
+            value={0}
+            control={<Radio color="primary" />}
+            label="Query by Example"
+          />
+          <FormControlLabel
+            value={1}
+            control={<Radio color="primary" />}
+            label="Apply Transfer Network"
+          />
         </RadioGroup>
       </FormControl>
       <div className={classes.window}>{childrenWithMoreProps}</div>
-      <div className={classes.matches}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={4}>
-            Viewing Matches for
-            {' '}
-            <MouseCoordinates position={mousePosition} />
-          </Grid>
-          <Grid item xs={12} md={4} className={classes.matchText}>
-            {matchesText}
-          </Grid>
-          <Grid item xs={12} md={4} className={classes.pagination}>
-            <Pagination count={pages} page={currentPage} onChange={handlePageChange} size="small" />
-          </Grid>
-        </Grid>
-        { isLoading ? 'Loading' : (
-          <Matches matches={paginatedList} imageRootUrl={imageRootUrl} actions={actions} />
-        )}
-      </div>
+      <div className={classes.matches}>{results}</div>
     </div>
   );
 }
---
+++ src/ImagePicker/TransferResults.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, shallowEqual } from 'react-redux';
+
+export default function TransferResults({ mousePosition, dataset, projectUrl }) {
+  const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
+  const [resultLink, setResultLink] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (mousePosition && mousePosition.length > 0 && user && dataset && projectUrl) {
+      const roundedPosition = mousePosition.map((point) => Math.floor(point));
+
+      setIsLoading(true);
+
+      const options = {
+        headers: {
+          Authorization: `Bearer ${user.getAuthResponse().id_token}`,
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          dataset: dataset.name,
+          model_name: dataset.transfer[0],
+          center: roundedPosition,
+        }),
+      };
+
+      const transferUrl = `${projectUrl}/transfer`;
+      console.log(options, transferUrl);
+      fetch(transferUrl, options)
+        .then((resp) => resp.json())
+        .then((result) => {
+          if ('addr' in result) {
+            setResultLink(result.addr);
+          }
+          setIsLoading(false);
+        });
+    }
+  }, [dataset, mousePosition, projectUrl, user]);
+
+  return (
+    <div>
+      <p>Transfer Results</p>
+      {isLoading ? <p>Loading</p> : <a href={resultLink}>View in neuroglancer</a>}
+    </div>
+  );
+}
+
+TransferResults.propTypes = {
+  mousePosition: PropTypes.arrayOf(PropTypes.number).isRequired,
+  dataset: PropTypes.object.isRequired,
+  projectUrl: PropTypes.string.isRequired,
+};
