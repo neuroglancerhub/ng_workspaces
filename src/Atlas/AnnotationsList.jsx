02955950619c174b554d1819785daf59a20e0d82
Jody Clements <jody@neomorphic.co.uk>
2020-09-01 16:19:08 -0400

    Atlas: Adds annotation selection pagination.

--- src/Atlas/AnnotationsList.jsx
+++ src/Atlas/AnnotationsList.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import Pagination from '@material-ui/lab/Pagination';
+
+const annotationsPerPage = 4;
 
 export default function AnnotationsList({ selected, onChange }) {
   const [annotations, setAnnotations] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setAnnotations([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
@@ -16,7 +20,18 @@ export default function AnnotationsList({ selected, onChange }) {
     onChange(name);
   };
 
-  const annotationSelections = annotations.map((annotation) => {
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const pages = Math.ceil(annotations.length / annotationsPerPage);
+
+  const paginatedAnnotations = annotations.slice(
+    currentPage * annotationsPerPage - annotationsPerPage,
+    currentPage * annotationsPerPage,
+  );
+
+  const annotationSelections = paginatedAnnotations.map((annotation) => {
     const name = annotation;
     return (
       <Grid key={name} item xs={12} sm={3}>
@@ -30,9 +45,12 @@ export default function AnnotationsList({ selected, onChange }) {
   });
 
   return (
-    <Grid container spacing={3}>
-      {annotationSelections}
-    </Grid>
+    <>
+      <Pagination count={pages} page={currentPage} onChange={handlePageChange} size="small" />
+      <Grid container spacing={3}>
+        {annotationSelections}
+      </Grid>
+    </>
   );
 }
 
