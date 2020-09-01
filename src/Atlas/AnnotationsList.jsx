aaef078d07f2df9c43104b3fd7855004c98ab3eb
Jody Clements <jody@neomorphic.co.uk>
2020-09-01 17:30:09 -0400

    Atlas: Adds annotations formatting with Card components.

--- src/Atlas/AnnotationsList.jsx
+++ src/Atlas/AnnotationsList.jsx
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  selected: {
+    border: '2px solid',
+    borderColor: theme.palette.primary.main,
+    background: 'rgba(143, 170, 143, 0.3)',
+  },
+}));
 
 const annotationsPerPage = 4;
 
 export default function AnnotationsList({ selected, onChange }) {
   const [annotations, setAnnotations] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const classes = useStyles();
 
   useEffect(() => {
     setAnnotations([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
@@ -35,11 +52,33 @@ export default function AnnotationsList({ selected, onChange }) {
     const name = annotation;
     return (
       <Grid key={name} item xs={12} sm={3}>
-        <button type="button" onClick={() => handleClick(name)}>
-          {selected === name && <p>Selected</p>}
-          <p>Annotation {name}</p>
-          <p>Show details and thumbnail</p>
-        </button>
+        <Card raised={selected === name} className={selected === name ? classes.selected : ''}>
+          <CardActionArea onClick={() => handleClick(name)}>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="140"
+              image="https://tensorslice-bmcp5imp6q-uk.a.run.app/slice/24344_16024_16728/256_128_1/jpeg?location=clio_mb20_raw_ng_r2_oldalign_7f8fc04a0929_v3/neuroglancer/jpeg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Annotation {name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Description text goes here.
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Data set.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" onClick={() => handleClick(name)}>
+              View
+            </Button>
+          </CardActions>
+        </Card>
       </Grid>
     );
   });
