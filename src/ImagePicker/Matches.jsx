5c13f94872b970a6458c383b59d274d0ce4ead89
Jody Clements <jody@neomorphic.co.uk>
2020-09-02 14:46:35 -0400

    ImagePicker: improves match display.

--- src/ImagePicker/Matches.jsx
+++ src/ImagePicker/Matches.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
 import MouseCoordinates from './MouseCoordinates';
 import './Matches.css';
 
@@ -17,19 +24,37 @@ export default function Matches({ matches, imageRootUrl, actions }) {
     const imageUrl = imageRootUrl.replace('<xyz>', xyzString);
     return (
       <Grid className="matchSlice" item xs={12} md={3} key={match.point}>
-        <MouseCoordinates position={match.point} />
-        <img src={imageUrl} alt={xyzString} />
-        <Button variant="outlined" color="primary" onClick={() => handleMatchSelect(match.point)}>
-          View in Context
-        </Button>
-        <Typography variant="body1" component="p">
-          Dist:
-          {match.dist}
-        </Typography>
-        <Typography variant="body1" component="p">
-          Score:
-          {match.score}
-        </Typography>
+        <Card raised>
+          <CardActionArea onClick={() => handleMatchSelect(match.point)}>
+            <CardMedia
+              component="img"
+              alt="match thumbnail"
+              height="256"
+              image={imageUrl}
+              title="match thumbnail"
+            />
+            <CardContent>
+              <MouseCoordinates position={match.point} />
+              <Typography variant="body1" component="p">
+                Distance:
+                {match.dist}
+              </Typography>
+              <Typography variant="body1" component="p">
+                Score:
+                {match.score}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => handleMatchSelect(match.point)}
+            >
+              View in Context
+            </Button>
+          </CardActions>
+        </Card>
       </Grid>
     );
   });
@@ -40,11 +65,9 @@ export default function Matches({ matches, imageRootUrl, actions }) {
     <div>
       <p>No matches found</p>
       <p>
-        To locate matches use neuroglancer to navigate to  a region of interest and
-        <span className="kbd">
-          {keyboardText}
-        </span>
-        + &apos;click&apos; on the point you are interested in.
+        To locate matches use neuroglancer to navigate to a region of interest and
+        <span className="kbd">{keyboardText}</span>+ &apos;click&apos; on the point you are
+        interested in.
       </p>
     </div>
   );
