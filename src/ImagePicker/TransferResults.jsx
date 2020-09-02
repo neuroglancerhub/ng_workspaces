b7341813286856d35d9af3da8e3b5122b9c4b77d
Jody Clements <jody@neomorphic.co.uk>
2020-09-02 15:45:29 -0400

    ImagePicker: transfer can now load multiple links

--- src/ImagePicker/TransferResults.jsx
+++ src/ImagePicker/TransferResults.jsx
@@ -1,6 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, shallowEqual } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import MouseCoordinates from './MouseCoordinates';
+
+const keyboardText = navigator.appVersion.indexOf('Mac') ? 'option' : 'alt';
+const noMatches = (
+  <div>
+    <p>No matches found</p>
+    <p>
+      To locate matches use neuroglancer to navigate to a region of interest and
+      <span className="kbd">{keyboardText}</span>+ &apos;click&apos; on the point you are
+      interested in.
+    </p>
+  </div>
+);
 
 export default function TransferResults({
   mousePosition,
@@ -9,14 +24,25 @@ export default function TransferResults({
   model,
 }) {
   const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
-  const [resultLink, setResultLink] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(0);
+  const [resultLinks, dispatch] = useReducer((searchList, { type, value }) => {
+    switch (type) {
+      case 'init':
+        return value;
+      case 'add':
+        return [...searchList, value];
+      case 'remove':
+        return searchList.filter((item) => item.id !== value);
+      default:
+        return searchList;
+    }
+  }, []);
 
   useEffect(() => {
     if (mousePosition && mousePosition.length > 0 && user && dataset && projectUrl) {
       const roundedPosition = mousePosition.map((point) => Math.floor(point));
 
-      setIsLoading(true);
+      setIsLoading((i) => i + 1);
 
       const options = {
         headers: {
@@ -31,22 +57,34 @@ export default function TransferResults({
       };
 
       const transferUrl = `${projectUrl}/transfer`;
-      console.log(options, transferUrl);
       fetch(transferUrl, options)
         .then((resp) => resp.json())
         .then((result) => {
           if ('addr' in result) {
-            setResultLink(result.addr);
+            const modifiedResult = {
+              coordinates: roundedPosition,
+              ...result,
+            };
+            dispatch({ type: 'add', value: modifiedResult });
           }
-          setIsLoading(false);
+          setIsLoading((i) => i - 1);
         });
     }
   }, [dataset, mousePosition, projectUrl, user, model]);
 
+  const linksList = resultLinks.map((link) => (
+    <li key={link.addr}>
+      <MouseCoordinates position={link.coordinates} />
+      <a href={link.addr}>View in Neuroglancer</a>
+    </li>
+  ));
+
   return (
     <div>
       <p>Transfer Results</p>
-      {isLoading ? <p>Loading</p> : <a href={resultLink}>View in neuroglancer</a>}
+      {isLoading === 0 && linksList.length === 0 && noMatches}
+      {isLoading > 0 && <CircularProgress />}
+      <ul>{linksList}</ul>
     </div>
   );
 }
