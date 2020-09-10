637cb04249c4c14d19ef484488ee71dc83a4273d
Jody Clements <jody@neomorphic.co.uk>
2020-09-10 10:47:35 -0400

    HomePage: Replaces JWT info with styled homepage.

--- src/About.jsx
+++ src/About.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
-import { addAlert } from './actions/alerts';
 
 function About() {
-  const dispatch = useDispatch();
-
-  const handleAlert = () => {
-    dispatch(
-      addAlert({
-        message: 'this is an alert message, that must be dismissed',
-      }),
-    );
-  };
-
-  const handleInfo = () => {
-    dispatch(
-      addAlert({
-        message: 'this is some information for quick display. Closes after 3 secs.',
-        severity: 'info',
-        duration: 3000,
-      }),
-    );
-  };
-
+  const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
   return (
-    <div className="homepage">
+    <div className="about">
       <Typography variant="h5">About</Typography>
-      <Button onClick={handleAlert}>Alert</Button>
-      <Button onClick={handleInfo}>Info</Button>
+      <span>v{process.env.REACT_APP_VERSION}</span>
+      {user && (
+        <>
+          <p>USER: {user.getBasicProfile().getName()}</p>
+          <p>JWT: {user.getAuthResponse().id_token}</p>
+        </>
+      )}
     </div>
   );
 }
