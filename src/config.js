31b7931a82bccc225379f8f1882dc27db2c534f2
Jody Clements <jody@neomorphic.co.uk>
2020-11-16 16:01:52 -0500

    Sets project to clio.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  project: 'FlyEM-Private',
+  project: 'Clio',
   google_auth: {
     // client_id: '833853795110-2eu65hnvthhcibk64ibftemb0i1tlu97.apps.googleusercontent.com',
     client_id: '464281314980-qlc4o2ce8htnhhgdb0g0j681de2l3980.apps.googleusercontent.com',
