cebd18bb557cc28a9e5f3ad6f14ef83f26df8ee0
Jody Clements <jody@neomorphic.co.uk>
2020-12-01 10:37:02 -0500

    Adds functionality to add and remove users.
    You can add and removes any user from the site, but the code still can't
    update or et permissions, other than the clio_general permission.

--- src/Admin/NewUserForm.jsx
+++ src/Admin/NewUserForm.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, shallowEqual } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import config from '../config';
 
-export default function NewUserForm() {
+const { project } = config;
+
+const formattedProject = project.toLowerCase().replace(/ /g, '-');
+
+export default function NewUserForm({ onUpdate }) {
+  const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
   const [userName, setUserName] = useState('');
   const [permissions, setPermissions] = useState('');
 
@@ -16,6 +24,31 @@ export default function NewUserForm() {
   const handleSubmit = () => {
     // TODO: validation
     // submit the data to the clio_toplevel/users end point.
+    const clioUrl = `https://us-east4-${formattedProject}.cloudfunctions.net/${
+      config.top_level_function
+    }`;
+
+    const usersUrl = `${clioUrl}/users`;
+
+    const userSettings = {};
+    userSettings[userName] = {
+      clio_global: ['clio_general'],
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${user.getAuthResponse().id_token}`,
+      },
+      body: JSON.stringify(userSettings),
+    };
+
+    fetch(usersUrl, options)
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        onUpdate(userSettings);
+      });
 
     console.log('submitting the data');
   };
@@ -44,3 +77,7 @@ export default function NewUserForm() {
     </form>
   );
 }
+
+NewUserForm.propTypes = {
+  onUpdate: PropTypes.func.isRequired,
+};
--- src/UserAdmin.jsx
+++ src/UserAdmin.jsx
@@ -6,14 +6,37 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import NewUserForm from './Admin/NewUserForm';
 import config from './config';
 
 const { project } = config;
 
+// set general users url for use by the handler functions
+const formattedProject = project.toLowerCase().replace(/ /g, '-');
+const clioUrl = `https://us-east4-${formattedProject}.cloudfunctions.net/${
+  config.top_level_function
+}`;
+const usersUrl = `${clioUrl}/users`;
+
+const useStyles = makeStyles((theme) => ({
+  error: {
+    width: '90%',
+    margin: `${theme.spacing(2)}px auto`,
+  },
+  userAdmin: {
+    margin: theme.spacing(2),
+  },
+}));
+
 function UserAdmin() {
+  const classes = useStyles();
   const user = useSelector((state) => state.user.get('googleUser'), shallowEqual);
   const [userList, setUserList] = useState({});
+  const [errorMsg, setErrorMsg] = useState(null);
   const [loadState, setLoadState] = useState('preload');
 
   useEffect(() => {
@@ -25,23 +48,54 @@ function UserAdmin() {
         },
       };
 
-      const formattedProject = project.toLowerCase().replace(/ /g, '-');
-
-      const clioUrl = `https://us-east4-${formattedProject}.cloudfunctions.net/${
-        config.top_level_function
-      }`;
-
-      const usersUrl = `${clioUrl}/users`;
-
       fetch(usersUrl, options)
-        .then((result) => result.json())
+        .then((result) => {
+          if (result.status !== 200) {
+            if (result.status === 403) {
+              throw new Error('Not authorized.');
+            }
+            throw new Error('Server response failed');
+          }
+          return result.json();
+        })
         .then((res) => {
           setUserList(res);
           setLoadState('loaded');
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          setErrorMsg(err.message);
+          setLoadState('failed');
+        });
     }
   }, [user]);
+
+  const handleDelete = (userName) => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${user.getAuthResponse().id_token}`,
+      },
+      body: JSON.stringify([userName]),
+    };
+    fetch(usersUrl, options)
+      .then((result) => result.text())
+      .then((res) => {
+        console.log(res);
+        setUserList((prevList) => {
+          const newList = { ...prevList };
+          delete newList[userName];
+          return newList;
+        });
+        console.log(`removed ${userName}`);
+      });
+  };
+
+  function handleNewUser(userInfo) {
+    setUserList((prevList) => {
+      const newList = { ...prevList, ...userInfo };
+      return newList;
+    });
+  }
 
   if (loadState === 'loading') {
     return (
@@ -51,28 +105,46 @@ function UserAdmin() {
     );
   }
 
+  if (loadState === 'failed') {
+    return (
+      <div className={classes.error}>
+        <Alert severity="error">Failed to load users list. {errorMsg}</Alert>
+      </div>
+    );
+  }
+
   const users = Object.entries(userList).map(([name]) => (
     <TableRow key={name}>
       <TableCell>{name}</TableCell>
-      <TableCell>Admin?</TableCell>
-      <TableCell>Delete | Update</TableCell>
+      <TableCell>{userList[name].clio_global.includes('admin') ? 'Admin' : 'General'}</TableCell>
+      <TableCell>
+        <Button onClick={() => handleDelete(name)}>Delete</Button> | <Button>Update</Button>
+      </TableCell>
     </TableRow>
   ));
 
   return (
-    <div className="userAdmin">
-      <Typography variant="h5">User Admin</Typography>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Username</TableCell>
-            <TableCell>Permissions</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>{users}</TableBody>
-      </Table>
-      <NewUserForm />
+    <div className={classes.userAdmin}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h5">User Admin</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Username</TableCell>
+                <TableCell>Permissions</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{users}</TableBody>
+          </Table>
+        </Grid>
+        <Grid item xs={12}>
+          <NewUserForm onUpdate={(userInfo) => handleNewUser(userInfo)} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
